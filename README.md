20cdf78578ce86118500c8fdd0e67868633d846e
LesterGL <79068516+LesterGL@users.noreply.github.com>
2021-03-23 10:08:44 -0600

    Lester Garcia
    no termine xd

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PropuestaSimposio
  Evaluación parcial de introducción a la ingeniería en informática y sistemas
+#######Evaluacion
+texto*creo que perdi*
+como dijo mi mama > de mal en peor 
+(https://docs.github.com/es/github/writing-on-github/basic-writing-and-formatting-syntax)
--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-int main()
-{
-  std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
-  std::cin.ignore();
+using namespace std;
+int main(){
+	int numero;
+	cout << "Propuesta de programa para el simposio de Ingenieria 2021\n" << endl;
+	cout << "ingrese cantidad de particioantes" << endl;
+	cin >> numero;
+
+
+
+
+ cin.ignore();
   return 0;
 }
